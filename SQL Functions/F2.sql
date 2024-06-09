b8285bd12a8bd735674371e48096a90569a680a2
77yuki <123405268+77yuki@users.noreply.github.com>
2024-06-09 16:05:54 -0400

    Add files via upload

---
+++ SQL Functions/F2.sql
@@ -0,0 +1,5 @@
+USE `cs 338 project`;
+SELECT `LAT`, `LON`
+FROM `cs 338 project`.sample
+WHERE `Crm Cd` = '{crime_type}';
+
---
+++ SQL Functions/TestF.sql
@@ -0,0 +1,7 @@
+USE `cs 338 project`;
+SELECT COUNT(*) AS number_of_victims
+FROM sample
+WHERE `Vict Age` BETWEEN '{ini_age}' AND '{end_age}'; 
+
+
+
