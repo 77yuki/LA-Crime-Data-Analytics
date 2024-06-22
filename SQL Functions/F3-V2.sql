fa59264b38b824cd1fbf89c9dbada15a6e2e75a1
hwz0116 <h247zhen@uwaterloo.ca>
2024-06-21 20:09:34 -0400

    Update F3-V2.sql

--- SQL Functions/F3-V2.sql
+++ SQL Functions/F3-V2.sql
@@ -6,7 +6,6 @@ UPDATE `cs 338 project`.sample
 SET `Vict Sex` = 'Others'
 WHERE `Vict Sex` = 'X';
 
-SELECT COUNT(*) AS total_others FROM `cs 338 project`.sample WHERE `Vict Sex` = 'Others';
 
 
 SET SQL_SAFE_UPDATES = 1;
