7d8f0247c16ccc48e53ea79132121615d58bf194
hwz0116 <h247zhen@uwaterloo.ca>
2024-07-06 18:02:17 -0400

    Add files via upload

---
+++ TA Please Check here/SQL Functions/F4.sql
@@ -0,0 +1,15 @@
+SELECT 
+    HOUR, 
+    COUNT(*) AS CrimeCount
+FROM (
+    SELECT 
+        FLOOR(`TIME OCC` / 100) AS HOUR
+    FROM 
+        `cs 338 project`.sample4
+    WHERE 
+        `AREA NAME` = '{area_name}'
+) AS HourlyCrimes
+GROUP BY 
+    HOUR
+ORDER BY 
+    HOUR;
