5e85eec9ba38b514477b65ad299cc28eb7f960f5
hwz0116 <h247zhen@uwaterloo.ca>
2024-07-06 18:24:21 -0400

    Add files via upload

---
+++ TA Please Check here/SQL Functions/Sample/F1_sampletestoutput.sql
@@ -0,0 +1,13 @@
+USE `cs 338 project`;
+
+CREATE TABLE F1 AS
+SELECT
+    t1.LAT,
+    t1.LON,
+    t2.`Crm Cd`
+FROM AREADIST t1
+JOIN TYPE t2 ON t1.DR_NO = t2.DR_NO;
+
+SELECT `LAT`, `LON`
+FROM F1
+WHERE `Crm Cd` = 756; #Use 756 to test
