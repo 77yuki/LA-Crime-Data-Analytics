6dd2b817635be545513940aaa2b1d195455ec002
77yuki <123405268+77yuki@users.noreply.github.com>
2024-07-07 08:22:28 -0400

    Add files via upload

---
+++ TA Please Check here/SQL Functions/Sample/F4_sampletestoutput.sql
@@ -0,0 +1,24 @@
+USE `cs 338 project`;
+
+CREATE TABLE F4 AS
+SELECT
+    t1.`AREA NAME`,
+    t2.`TIME OCC`
+FROM AREADIST t1
+JOIN DATETIME t2 ON t1.DR_NO = t2.DR_NO;
+
+SELECT 
+    HOUR, 
+    COUNT(*) AS CrimeCount
+FROM (
+    SELECT 
+        FLOOR(`TIME OCC` / 100) AS HOUR
+    FROM 
+        F4
+    WHERE 
+        `AREA NAME` = 'Newton' #Use Newton to test
+) AS HourlyCrimes
+GROUP BY 
+    HOUR
+ORDER BY 
+    HOUR;
