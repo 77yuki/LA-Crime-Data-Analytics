228bb4b830ebd7e913f67b7f84e53eeb532fe35d
hwz0116 <h247zhen@uwaterloo.ca>
2024-07-07 09:36:34 -0400

    Update F6.sql

--- TA Please Check here/SQL Functions/F6.sql
+++ TA Please Check here/SQL Functions/F6.sql
@@ -7,6 +7,6 @@ FROM (
     SELECT 
         DR_NO, 
         (6371000 * ACOS(COS(RADIANS(@target_lat)) * COS(RADIANS(LAT)) * COS(RADIANS(LON) - RADIANS(@target_lon)) + SIN(RADIANS(@target_lat)) * SIN(RADIANS(LAT)))) AS distance
-    FROM areadist_sample_table
+    FROM areadist_table
 ) AS distances
 WHERE distance <= @radius;
