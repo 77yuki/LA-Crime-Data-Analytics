5b11a53be601f8eb0443ed87c78d902ff0df03e1
77yuki <123405268+77yuki@users.noreply.github.com>
2024-07-07 23:24:21 -0400

    Update F4_sampletestoutput.sql

--- TA Please Check here/SQL Functions/Sample/F4_sampletestoutput.sql
+++ TA Please Check here/SQL Functions/Sample/F4_sampletestoutput.sql
@@ -1,24 +1,38 @@
 USE `cs 338 project`;
 
-CREATE TABLE F4 AS
+CREATE TABLE IF NOT EXISTS Hours (
+    HOUR INT PRIMARY KEY
+);
+
+INSERT INTO Hours (HOUR)
+VALUES 
+(0), (1), (2), (3), (4), (5), (6), (7), (8), (9), (10), 
+(11), (12), (13), (14), (15), (16), (17), (18), 
+(19), (20), (21), (22), (23);
+
+CREATE TABLE IF NOT EXISTS F4 AS
 SELECT
     t1.`AREA NAME`,
     t2.`TIME OCC`
-FROM AREADIST_TABLE t1
-JOIN DATETIME_TABLE t2 ON t1.DR_NO = t2.DR_NO;
+FROM AREADIST t1
+JOIN DATETIME t2 ON t1.DR_NO = t2.DR_NO;
 
 SELECT 
-    HOUR, 
-    COUNT(*) AS CrimeCount
-FROM (
+    h.HOUR,
+    COALESCE(CrimeCount, 0) AS CrimeCount
+FROM 
+    Hours h
+LEFT JOIN (
     SELECT 
-        FLOOR(`TIME OCC` / 100) AS HOUR
+        FLOOR(`TIME OCC` / 100) AS HOUR,
+        COUNT(*) AS CrimeCount
     FROM 
         F4
     WHERE 
-        `AREA NAME` = 'Newton' #Use Newton to test
-) AS HourlyCrimes
-GROUP BY 
-    HOUR
+        `AREA NAME` = 'Newton'  #Use 'Newton' to test
+    GROUP BY 
+        HOUR
+) AS HourlyCrimes ON h.HOUR = HourlyCrimes.HOUR
 ORDER BY 
-    HOUR;
+    h.HOUR;
+
