c2b47e713495c509cf84f1ccdf8e2f96d1948de4
77yuki <123405268+77yuki@users.noreply.github.com>
2024-07-08 23:32:56 -0400

    Add files via upload

---
+++ Milestone 2/SQL Functions/Sample/F4_sampletest2.sql
@@ -0,0 +1,37 @@
+USE `cs 338 project`;
+
+CREATE TABLE Hours (
+    HOUR INT PRIMARY KEY
+);
+
+INSERT INTO Hours (HOUR)
+VALUES 
+(0), (1), (2), (3), (4), (5), (6), (7), (8), (9), (10), 
+(11), (12), (13), (14), (15), (16), (17), (18), 
+(19), (20), (21), (22), (23);
+
+CREATE TABLE F4 AS
+SELECT
+    t1.`AREA NAME`,
+    t2.`TIME OCC`
+FROM AREADIST_TABLE t1
+JOIN DATETIME_TABLE t2 ON t1.DR_NO = t2.DR_NO;
+
+SELECT 
+    h.HOUR,
+    COALESCE(CrimeCount, 0) AS CrimeCount
+FROM 
+    Hours h
+LEFT JOIN (
+    SELECT 
+        FLOOR(`TIME OCC` / 100) AS HOUR,
+        COUNT(*) AS CrimeCount
+    FROM 
+        F4
+    WHERE 
+        `AREA NAME` = 'Hollywood'  
+    GROUP BY 
+        HOUR
+) AS HourlyCrimes ON h.HOUR = HourlyCrimes.HOUR
+ORDER BY 
+    h.HOUR;
