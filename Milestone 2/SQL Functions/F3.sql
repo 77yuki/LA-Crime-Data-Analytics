af077ac186dcca86d23daf890ced7547769aa12b
77yuki <123405268+77yuki@users.noreply.github.com>
2024-07-08 23:11:41 -0400

    Update F3.sql

--- Milestone 2/SQL Functions/F3.sql
+++ Milestone 2/SQL Functions/F3.sql
@@ -10,4 +10,4 @@ JOIN DATETIME_TABLE t2 ON t1.DR_NO = t2.DR_NO;
 
 SELECT `LAT`, `LON`
 FROM F3
-WHERE `DATE OCC` BETWEEN '{ini_date}' AND '{end_date}'; #Use 04/06/2020 and 04/25/2020 to test
+WHERE `DATE OCC` BETWEEN '{ini_date}' AND '{end_date}'; #Use 04/06/2020 and 04/25/2020 to test to get F3_sampleoutput.out, and use '03/24/2020' AND '06/24/2020' to test and get F3_sampleoutput2.out
