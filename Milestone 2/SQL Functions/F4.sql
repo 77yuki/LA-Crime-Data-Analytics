3b67838d19a7fa396d27ae025c11c9415719dca7
hwz0116 <h247zhen@uwaterloo.ca>
2024-07-21 22:59:55 -0400

    Update F4.sql

--- Milestone 2/SQL Functions/F4.sql
+++ Milestone 2/SQL Functions/F4.sql
@@ -1,41 +1,49 @@
 USE `cs 338 project`;
 
-CREATE TABLE Hours (
-    HOUR INT PRIMARY KEY
-);
-
-INSERT INTO Hours (HOUR)
-VALUES 
-(0), (1), (2), (3), (4), (5), (6), (7), (8), (9), (10), 
-(11), (12), (13), (14), (15), (16), (17), (18), 
-(19), (20), (21), (22), (23);
-
-CREATE TABLE F4 AS
-SELECT
-    t1.`AREA NAME`,
-    t2.`TIME OCC`
-FROM AREADIST_TABLE t1
-JOIN DATETIME_TABLE t2 ON t1.DR_NO = t2.DR_NO;
 
 SELECT 
     h.HOUR,
     COALESCE(CrimeCount, 0) AS CrimeCount
 FROM 
-    Hours h
+    (SELECT 0 AS HOUR UNION ALL
+     SELECT 1 UNION ALL
+     SELECT 2 UNION ALL
+     SELECT 3 UNION ALL
+     SELECT 4 UNION ALL
+     SELECT 5 UNION ALL
+     SELECT 6 UNION ALL
+     SELECT 7 UNION ALL
+     SELECT 8 UNION ALL
+     SELECT 9 UNION ALL
+     SELECT 10 UNION ALL
+     SELECT 11 UNION ALL
+     SELECT 12 UNION ALL
+     SELECT 13 UNION ALL
+     SELECT 14 UNION ALL
+     SELECT 15 UNION ALL
+     SELECT 16 UNION ALL
+     SELECT 17 UNION ALL
+     SELECT 18 UNION ALL
+     SELECT 19 UNION ALL
+     SELECT 20 UNION ALL
+     SELECT 21 UNION ALL
+     SELECT 22 UNION ALL
+     SELECT 23) AS h
 LEFT JOIN (
     SELECT 
-        FLOOR(`TIME OCC` / 100) AS HOUR,
+        FLOOR(t2.`TIME OCC` / 100) AS HOUR,
         COUNT(*) AS CrimeCount
     FROM 
-        F4
+        AREADIST_TABLE t1
+    JOIN 
+        DATETIME_TABLE t2 ON t1.DR_NO = t2.DR_NO
     WHERE 
-        `AREA NAME` = '{area_name}'  #Use 'Newton' to test to get F4_sampleoutput.out, and use 'Hollywood' to test to get F4_sampleoutput2.out 
+        t1.`AREA NAME` = '{area_name}'  #Use 'Newton' to test to get F4_sampleoutput.out, and use 'Hollywood' to test to get F4_sampleoutput2.out   
     GROUP BY 
-        HOUR
+        FLOOR(t2.`TIME OCC` / 100)
 ) AS HourlyCrimes ON h.HOUR = HourlyCrimes.HOUR
 ORDER BY 
     h.HOUR;
-
 
 
 
