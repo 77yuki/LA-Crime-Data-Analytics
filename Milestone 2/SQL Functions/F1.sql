33324d94d564d28570e3ac0c0385858d593633c4
hwz0116 <h247zhen@uwaterloo.ca>
2024-07-21 21:29:53 -0400

    Update F1.sql

--- Milestone 2/SQL Functions/F1.sql
+++ Milestone 2/SQL Functions/F1.sql
@@ -1,15 +1,7 @@
+
 USE `cs 338 project`;
 
-CREATE TABLE F1 AS
-SELECT
-    t1.LAT,
-    t1.LON,
-    t2.`Crm Cd`
+SELECT t1.LAT, t1.LON
 FROM AREADIST_TABLE t1
-JOIN TYPE_TABLE t2 ON t1.DR_NO = t2.DR_NO;
-
-SELECT `LAT`, `LON`
-FROM F1
-WHERE `Crm Cd` = '{crime_code}'; #Use 756 to test to get F1_sampleoutput.out, and use 420 to test and get F1_sampleoutput2
-
-
+JOIN TYPE_TABLE t2 ON t1.DR_NO = t2.DR_NO
+WHERE t2.`Crm Cd` = '{crime_code}'; #Use 756 to test to get F1_sampleoutput.out, and use 420 to test and get F1_sampleoutput2
