3f1b1e7184dc87fcfbcfe1ad00e551f5e2e80b5e
77yuki <123405268+77yuki@users.noreply.github.com>
2024-07-23 18:13:52 -0400

    Update F4.sql

--- Milestone 2/SQL Functions/F4.sql
+++ Milestone 2/SQL Functions/F4.sql
@@ -1,33 +1,16 @@
 USE `cs 338 project`;
 
+WITH RECURSIVE Hours AS (
+    SELECT 0 AS HOUR
+    UNION ALL
+    SELECT HOUR + 1 FROM Hours WHERE HOUR < 23
+)
+    
 SELECT 
     h.HOUR,
     COALESCE(CrimeCount, 0) AS CrimeCount
 FROM 
-    (SELECT 0 AS HOUR UNION ALL
-     SELECT 1 UNION ALL
-     SELECT 2 UNION ALL
-     SELECT 3 UNION ALL
-     SELECT 4 UNION ALL
-     SELECT 5 UNION ALL
-     SELECT 6 UNION ALL
-     SELECT 7 UNION ALL
-     SELECT 8 UNION ALL
-     SELECT 9 UNION ALL
-     SELECT 10 UNION ALL
-     SELECT 11 UNION ALL
-     SELECT 12 UNION ALL
-     SELECT 13 UNION ALL
-     SELECT 14 UNION ALL
-     SELECT 15 UNION ALL
-     SELECT 16 UNION ALL
-     SELECT 17 UNION ALL
-     SELECT 18 UNION ALL
-     SELECT 19 UNION ALL
-     SELECT 20 UNION ALL
-     SELECT 21 UNION ALL
-     SELECT 22 UNION ALL
-     SELECT 23) AS h
+    Hours h
 LEFT JOIN (
     SELECT 
         FLOOR(t2.`TIME OCC` / 100) AS HOUR,
@@ -37,14 +20,13 @@ LEFT JOIN (
     JOIN 
         DATETIME_TABLE t2 ON t1.DR_NO = t2.DR_NO
     JOIN
-        STATUS_TBALE t3 ON t1.DR_NO = t3.DR_NO
+        STATUS_TABLE t3 ON t1.DR_NO = t3.DR_NO
     WHERE 
-        t1.`AREA NAME` = '{area_name}'  #Use 'Newton' to test to get F4_sampleoutput.out, and use 'Hollywood' to test to get F4_sampleoutput2.out   
-        AND t3.`Status` = '{status}' #For example 'IC'
+        t1.`AREA NAME` = 'Newton'   #Use 'Newton' to test to get F4_sampleoutput.out, and use 'Hollywood' to test to get F4_sampleoutput2.out  
+        AND t3.`Status` = 'IC'    #Use 'IC' to test
     GROUP BY 
         FLOOR(t2.`TIME OCC` / 100)
 ) AS HourlyCrimes ON h.HOUR = HourlyCrimes.HOUR
 ORDER BY 
     h.HOUR;
-
 
