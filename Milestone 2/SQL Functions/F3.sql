2a91a3a44792ab4c17411f47fbb6e003d9366ecd
77yuki <123405268+77yuki@users.noreply.github.com>
2024-07-23 18:29:42 -0400

    Update F3.sql

--- Milestone 2/SQL Functions/F3.sql
+++ Milestone 2/SQL Functions/F3.sql
@@ -4,5 +4,5 @@ SELECT `LAT`, `LON`
 FROM AREADIST_TABLE t1
 JOIN DATETIME_TABLE t2 ON t1.DR_NO = t2.DR_NO
 JOIN STATUS_TABLE t3 ON t1.DR_NO = t3.DR_NO
-WHERE t2.`DATE OCC` BETWEEN '04/06/2020' AND '04/25/2020' #Use '04/06/2020' AND '04/25/2020' to test
-  AND t3.`Status` = 'IC'; #Use 'IC' to test
+WHERE t2.`DATE OCC` BETWEEN '{init_date}' AND '{end_date}' #Use '04/06/2020' AND '04/25/2020' to test
+  AND t3.`Status` = '{status}'; #Use 'IC' to test
