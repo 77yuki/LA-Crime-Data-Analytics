91d02244549313c5dae07c2e215970749d249acd
77yuki <123405268+77yuki@users.noreply.github.com>
2024-07-24 13:55:13 -0400

    Update F3.sql

--- Milestone 2/SQL Functions/F3.sql
+++ Milestone 2/SQL Functions/F3.sql
@@ -3,5 +3,5 @@ USE `cs 338 project`;
 SELECT `LAT`, `LON`
 FROM AREADIST_TABLE t1
 JOIN STATUS_TABLE t2 ON t1.DR_NO = t2.DR_NO
-WHERE t1.`AREA NAME` = 'West LA' 
-  AND t2.`Status` = 'IC'; #Use 'West LA', IC' to test
+WHERE t1.`AREA NAME` = '{area_name}' 
+  AND t2.`Status` = '{status}'; #Use 'West LA', IC' to test
