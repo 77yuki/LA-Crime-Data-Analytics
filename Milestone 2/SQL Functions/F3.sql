73b1a63407b62b05607510c90d06ffd4e4196487
77yuki <123405268+77yuki@users.noreply.github.com>
2024-07-24 13:54:06 -0400

    Update F3.sql

--- Milestone 2/SQL Functions/F3.sql
+++ Milestone 2/SQL Functions/F3.sql
@@ -2,7 +2,6 @@ USE `cs 338 project`;
 
 SELECT `LAT`, `LON`
 FROM AREADIST_TABLE t1
-JOIN DATETIME_TABLE t2 ON t1.DR_NO = t2.DR_NO
-JOIN STATUS_TABLE t3 ON t1.DR_NO = t3.DR_NO
-WHERE t2.`DATE OCC` BETWEEN '{init_date}' AND '{end_date}' #Use '04/06/2020' AND '04/25/2020' to test
-  AND t3.`Status` = '{status}'; #Use 'IC' to test
+JOIN STATUS_TABLE t2 ON t1.DR_NO = t2.DR_NO
+WHERE t1.`AREA NAME` = 'West LA' 
+  AND t2.`Status` = 'IC'; #Use 'West LA', IC' to test
