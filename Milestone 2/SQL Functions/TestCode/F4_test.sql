39992394e9b022b78c36db668aa0017c157503e2
hwz0116 <h247zhen@uwaterloo.ca>
2024-07-31 01:19:44 -0400

    Update F4_test.sql

--- Milestone 2/SQL Functions/TestCode/F4_test.sql
+++ Milestone 2/SQL Functions/TestCode/F4_test.sql
@@ -1,21 +1,10 @@
 USE `cs 338 project`;
 
-CREATE TABLE IF NOT EXISTS Hours (
-    HOUR INT PRIMARY KEY
-);
-
-INSERT INTO Hours (HOUR)
-VALUES 
-(0), (1), (2), (3), (4), (5), (6), (7), (8), (9), (10), 
-(11), (12), (13), (14), (15), (16), (17), (18), 
-(19), (20), (21), (22), (23);
-
-CREATE TABLE IF NOT EXISTS F4 AS
-SELECT
-    t1.`AREA NAME`,
-    t2.`TIME OCC`
-FROM AREADIST_TABLE t1
-JOIN DATETIME_TABLE t2 ON t1.DR_NO = t2.DR_NO;
+WITH RECURSIVE Hours AS (
+    SELECT 0 AS HOUR
+    UNION ALL
+    SELECT HOUR + 1 FROM Hours WHERE HOUR < 23
+)
 
 SELECT 
     h.HOUR,
@@ -24,15 +13,18 @@ FROM
     Hours h
 LEFT JOIN (
     SELECT 
-        FLOOR(`TIME OCC` / 100) AS HOUR,
+        FLOOR(t2.`TIME OCC` / 100) AS HOUR,
         COUNT(*) AS CrimeCount
     FROM 
-        F4
+        AREADIST_TABLE t1
+    JOIN 
+        DATETIME_TABLE t2 ON t1.DR_NO = t2.DR_NO
     WHERE 
-        `AREA NAME` = 'Newton'  #Use 'Newton' to test
+        t1.`AREA NAME` = 'Newton'     
     GROUP BY 
-        HOUR
+        FLOOR(t2.`TIME OCC` / 100)
 ) AS HourlyCrimes ON h.HOUR = HourlyCrimes.HOUR
 ORDER BY 
     h.HOUR;
+
 
