d371a78fdd64c82d2f410da6e7013027ad83e7bf
hwz0116 <h247zhen@uwaterloo.ca>
2024-07-30 21:00:52 -0400

    Update F1.sql

--- Milestone 2/SQL Functions/F1.sql
+++ Milestone 2/SQL Functions/F1.sql
@@ -3,4 +3,4 @@ USE `cs 338 project`;
 SELECT t1.LAT, t1.LON
 FROM AREADIST_TABLE t1
 JOIN TYPE_TABLE t2 ON t1.DR_NO = t2.DR_NO
-WHERE t2.`Crm Cd` = '{crime_code}'; #Use 756 to test to get F1_sampleoutput.out, and use 420 to test and get F1_sampleoutput2
+WHERE t2.`Crm Cd` = '{crime_code}'; #Use 420 to test
