a83239a67807ffa369756c817c790d8f2824fd62
hwz0116 <h247zhen@uwaterloo.ca>
2024-07-30 21:03:25 -0400

    Update F3.sql

--- Milestone 2/SQL Functions/F3.sql
+++ Milestone 2/SQL Functions/F3.sql
@@ -4,4 +4,4 @@ SELECT `LAT`, `LON`
 FROM AREADIST_TABLE t1
 JOIN STATUS_TABLE t2 ON t1.DR_NO = t2.DR_NO
 WHERE t1.`AREA NAME` = '{area_name}' 
-  AND t2.`Status` = '{status}'; #Use 'West LA', IC' to test
+  AND t2.`Status` = '{status}'; #Use 'West LA', 'AO' to test
