9694ee6fed9b1e367e235b268510f9cba5166de6
hwz0116 <h247zhen@uwaterloo.ca>
2024-07-31 01:21:02 -0400

    Update F6_test.sql

--- Milestone 2/SQL Functions/TestCode/F6_test.sql
+++ Milestone 2/SQL Functions/TestCode/F6_test.sql
@@ -1,12 +1,13 @@
-SET @target_lat = 34.052235;  
-SET @target_lon = -118.243683; 
+SET @target_lat = 34.1018;  
+SET @target_lon = -118.3035; 
 SET @radius = 500;  
 
-SELECT COUNT(*) AS crime_count
+
+SELECT @target_lat AS LAT, @target_lon AS LON, COUNT(*) AS crime_count
 FROM (
     SELECT 
         DR_NO, 
         (6371000 * ACOS(COS(RADIANS(@target_lat)) * COS(RADIANS(LAT)) * COS(RADIANS(LON) - RADIANS(@target_lon)) + SIN(RADIANS(@target_lat)) * SIN(RADIANS(LAT)))) AS distance
     FROM areadist_table
 ) AS distances
-WHERE distance <= @radius;
+WHERE distance <= @radius; 
